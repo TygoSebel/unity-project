33ac607c1f98d037b7f3f7198eac5e6abfb8c846
TygoSebel <9028035@student.zadkine.nl>
2026-03-10 12:53:27 +0100

    Week 2 t/m video 13

--- My project/Assets/Scripts/Mover.cs
+++ My project/Assets/Scripts/Mover.cs
@@ -3,19 +3,31 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
-    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float moveSpeed = 20f;
+    
     void Start()
     {
-        
+        PrintInstructions();
     }
 
     
     void Update()
     {
+        MovePlayer();
+    }
+
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to the game!");
+        Debug.Log("Use WASD or Arrow Keys to move.");
+        Debug.Log("Don't bump into the objects!");
+    }
+
+    void MovePlayer()
+    {
         float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float yValue = 0f;
         float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
         transform.Translate(xValue, yValue, zValue);
-    
     }
 }
